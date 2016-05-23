0a6143a1352e938ad5c23ed34573224e394c3f13
John Heher <jheher@gmail.com>
2016-05-22 23:51:32 -0500

    Cleanup of old files

--- client/components/CountryCard.js
+++ client/components/CountryCard.js
@@ -6,23 +6,30 @@ import Flag from "./Flag";
 export default class CountryCard extends React.Component {
 
   render() {
-    const userId = this.props.currentUser.id
-    const { id, selected, name } = this.props.country
-    const selectedClass = (this.props.country.userId === userId) ? "selected" : ""
-    const disabled = (!selected && this.props.regionCompleted || (this.props.country.userId && this.props.country.userId !== userId)) ? "disabled" : ""
+    const currentUserId = this.props.currentUser.id
+    const { id, selected, name, userId } = this.props.country
+
+    const available = userId || this.props.regionCompleted ? false : true
+    const ownedByCurrentUser = (currentUserId === userId) ? true : false
+
+    const disabled = !available && !ownedByCurrentUser ? true : false
+
+    const canBeChanged = available || ownedByCurrentUser ? true : false
+
+    const ownedClass = userId && !ownedByCurrentUser ? "owned" : ""
+    const selectedClass = ownedByCurrentUser ? "selected" : ""
+    const disabledClass = disabled ? "disabled" : ""
 
     if (selected) {
       return (
-        <button className = {`countryCard ${selectedClass} ${disabled}`} onClick={this.props.deselectingCountry.bind(null, this.props.region.id, id, userId, disabled)} >
-          <h3>{name}</h3>
-          <Flag country={this.props.country}/>
+        <button className = {`countryCard ${ownedClass} ${selectedClass} ${disabledClass}`} onClick={this.props.deselectingCountry.bind(null, this.props.region.id, id, userId, !canBeChanged)} >
+          <Flag country={this.props.country}/><h3>{name}</h3>
         </button>
       )
     } else {
       return (
-        <button className = {`countryCard ${selectedClass} ${disabled}`} onClick={this.props.selectingCountry.bind(null, this.props.region.id, id, userId, disabled)} >
-          <h3>{name}</h3>
-          <Flag country={this.props.country}/>
+        <button className = {`countryCard ${ownedClass} ${selectedClass} ${disabledClass}`} onClick={this.props.selectingCountry.bind(null, this.props.region.id, id, currentUserId, !canBeChanged)} >
+          <Flag country={this.props.country}/><h3>{name}</h3>
         </button>
       )
     }
--- client/store.js
+++ client/store.js
@@ -14,8 +14,19 @@ const defaultState = {
   users
 }
 
-const store = createStore(rootReducer, defaultState)
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
+const store = createStore(rootReducer, defaultState, enhancers)
 
 export const history = syncHistoryWithStore(browserHistory, store)
+
+if(module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
 
 export default store
