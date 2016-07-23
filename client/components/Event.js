d3d4320d250d224568a02331568cff5ca3ed333d
John Heher <jheher@gmail.com>
2016-07-22 20:33:32 -0500

    Adds and styles editing view

--- client/components/Event.js
+++ client/components/Event.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import DateTime from 'react-datetime'
 import moment from 'moment'
 import classNames from 'classnames'
 
 import Flag from './Flag'
 import Avatar from './Avatar'
+import EventCountrySelect from './admin/events/EventCountrySelect'
+import DeleteItem from './admin/panel/buttons/DeleteItem'
+import SaveItem from './admin/panel/buttons/SaveItem'
 
 require('../css/events.sass')
+require('../css/inputs/country_select.sass')
 
 export default class Event extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      editing: false,
+      inputValue: this.props.event.name,
+      dateValue: new Date(this.props.event.datetime),
+      checkboxValue: this.props.event.team
+    }
   }
 
   findCountry(country) {
@@ -63,8 +74,62 @@ export default class Event extends React.Component {
     return moment(datetime, moment.ISO_8601).format("ddd, M/D, h:mm A")
   }
 
+  handleEditToggle() {
+    this.setState({
+      editing: !this.state.editing
+    })
+  }
+
+  handleInputChange(event) {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleCheckboxChange(event) {
+    this.setState({checkboxValue: event.target.checked})
+  }
+
+  sortCountryOptions(countries) {
+    if (countries.length) {
+      return countries.sort(function(a, b) {
+        if(a.name < b.name) return -1
+        if(a.name > b.name) return 1
+        return 0
+      })
+    } else {
+      return null
+    }
+  }
+
+  renderSelects(countries, type) {
+    let selects = []
+    let selectOptions = []
+
+    const sortedOptions = this.sortCountryOptions(this.props.countries)
+    sortedOptions.map(country => {
+      selectOptions.push({'value': country.name, 'label': country.name})
+    })
+    if (countries.length > 0) {
+      countries.map((country, index) => {
+        selects.push(
+          <EventCountrySelect {...this.props} key={index} country={country} type={type} handleAddMedal={this.handleAddMedal.bind(this)} />
+        )
+      })
+    } else {
+      selects.push(<EventCountrySelect {...this.props} key={0} type={type} />)
+    }
+    return selects
+  }
+
+  handleAddMedal() {
+    console.log("Add")
+  }
+
+  handleItemSave() {
+    console.log("Saved")
+  }
+
   render() {
-    const { event } = this.props
+    const { event, currentUser } = this.props
 
     const goldCountries = event.gold.map((country, index) => {
       return this.findCountry(country)
@@ -76,25 +141,65 @@ export default class Event extends React.Component {
       return this.findCountry(country)
     })
 
-    return (
-      <div className="event-section">
-        <div className="title">
-          <h4>{event.name}</h4>
-          {event.datetime ? <p>{this.convertDate(event.datetime)}</p> : null}
-          {event.team ? <span className="team-badge">Team</span> : null}
+    if (this.state.editing) {
+      return (
+        <div className="event-section editing">
+          <div className="title">
+            {currentUser.isAdmin ? <button className="edit-button" onClick={this.handleEditToggle.bind(this)}>Cancel</button> : null}
+            <input
+              type="text"
+              onChange={this.handleInputChange.bind(this)}
+              value={this.state.inputValue}
+            />
+            <DateTime defaultValue={this.state.dateValue} timeFormat="HH:mm"/>
+            <div className="admin-checkbox">
+              <input
+                type="checkbox"
+                checked={this.state.checkboxValue ? "checked" : ""}
+                onChange={this.handleCheckboxChange.bind(this)}
+              />
+              <p>Team Event</p>
+            </div>
+          </div>
+          <div className="medal-winners">
+            <div className="golds">
+              {this.renderSelects(goldCountries, "gold")}
+            </div>
+            <div className="silvers">
+              {this.renderSelects(silverCountries, "silver")}
+            </div>
+            <div className="bronzes">
+              {this.renderSelects(bronzeCountries, "bronze")}
+            </div>
+          </div>
+          <div className="action-buttons">
+            <SaveItem {...this.props} item={event} type="Event" handleItemSave={this.handleItemSave.bind(this)}/>
+            <DeleteItem {...this.props} item={event} type="Event" />
+          </div>
         </div>
-        <div className="medal-winners">
-          <div className="golds">
-            {this.renderWinners(goldCountries)}
+      )
+    } else {
+      return (
+        <div className="event-section">
+          <div className="title">
+            {currentUser.isAdmin ? <button className="edit-button" onClick={this.handleEditToggle.bind(this)}>Edit</button> : null}
+            <h4>{event.name}</h4>
+            {event.datetime ? <p>{this.convertDate(event.datetime)}</p> : null}
+            {event.team ? <span className="team-badge">Team</span> : null}
           </div>
-          <div className="silvers">
-            {this.renderWinners(silverCountries)}
-          </div>
-          <div className="bronzes">
-            {this.renderWinners(bronzeCountries)}
+          <div className="medal-winners">
+            <div className="golds">
+              {this.renderWinners(goldCountries)}
+            </div>
+            <div className="silvers">
+              {this.renderWinners(silverCountries)}
+            </div>
+            <div className="bronzes">
+              {this.renderWinners(bronzeCountries)}
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
--- client/components/admin/events/EventCountrySelect.js
+++ client/components/admin/events/EventCountrySelect.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import classNames from 'classnames'
+import Autosuggest from 'react-autosuggest'
+
+import Flag from '../../Flag'
+
+require('../../../css/inputs/country_select.sass')
 
 export default class EventCountrySelect extends React.Component {
   constructor(props) {
     super(props)
     if (this.props.country) {
       this.state = {
-        countryValue: this.props.country._id
+        countryValue: this.props.country.name,
+        suggestions: this.getSuggestions(''),
+        selectedCountry: this.props.country
       }
     } else {
       this.state = {
-        countryValue: ''
+        countryValue: '',
+        suggestions: this.getSuggestions(''),
+        selectedCountry: ''
       }
     }
+    this.onChange = this.onChange.bind(this)
+    this.onSuggestionsUpdateRequested = this.onSuggestionsUpdateRequested.bind(this)
+    this.onSuggestionSelected = this.onSuggestionSelected.bind(this)
+  }
+
+  escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  }
+
+  getSuggestions(value) {
+    const escapedValue = this.escapeRegexCharacters(value.trim())
+    
+    if (escapedValue === '') {
+      return [];
+    }
+
+    const regex = new RegExp('\\b' + escapedValue, 'i')
+    
+    return this.props.countries.filter(country => regex.test(this.getSuggestionValue(country)))
   }
 
   sortCountryOptions(countries) {
@@ -28,11 +56,47 @@ export default class EventCountrySelect extends React.Component {
   }
 
   handleSelectChange(event) {
-    this.setState({countryValue: event.target.value})
+    this.setState({
+      countryValue: event.target.value,
+    })
+  }
+
+  getSuggestionValue(suggestion) {
+    return suggestion.name
+  }
+
+  renderSuggestion(suggestion) {
+    return (
+      <span><Flag country={suggestion} />{suggestion.name}</span>
+    )
+  }
+
+  onChange(event, { newValue }) {
+    this.setState({
+      countryValue: newValue
+    })
+  }
+
+  onSuggestionsUpdateRequested({ value }) {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    })
+  }
+
+  onSuggestionSelected(event, { suggestion, suggestionValue, sectionIndex, method }) {
+    this.setState({
+      selectedCountry: suggestion
+    })
   }
 
   render() {
     const { type, countries, country } = this.props
+
+    const inputProps = {
+      placeholder: 'Select Country',
+      value: this.state.countryValue,
+      onChange: this.onChange
+    }
 
     let countryOptions = []
 
@@ -48,11 +112,18 @@ export default class EventCountrySelect extends React.Component {
     })
 
     return (
-      <div className={winnerClass}>
-        <span>&nbsp;</span>
-        <select className={type} onChange={this.handleSelectChange.bind(this)} value={this.state.countryValue}>
-          {countryOptions}
-        </select>
+      <div className={`event-select ${type}`}>
+        <span className="medal add-medal" onClick={this.props.handleAddMedal.bind(this)}>+</span>
+        <Flag country={this.state.selectedCountry} />
+        <Autosuggest
+          className={winnerClass} 
+          suggestions={this.state.suggestions}
+          onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
+          onSuggestionSelected={this.onSuggestionSelected}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={this.renderSuggestion}
+          inputProps={inputProps}
+        />
       </div>
     )
   }
