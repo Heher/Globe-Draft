ceb258de31798c75489245d77d792d8352a41ae0
John Heher <jheher@gmail.com>
2016-08-07 17:45:38 -0500

    Adds user country details to sidebar leaderboard

---
+++ client/components/UserCountryPoints.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import Flag from './Flag'
+
+export default class UserCountryPoints extends React.Component {
+  findUserCountries(userId) {
+    return this.props.countries.filter(country => {
+      return country.userId === userId
+    })
+  }
+
+  sumCountry(country) {
+    let sum = 0
+    this.props.events.map(event => {
+      event.gold.map(gold => {
+        if (gold.id === country._id) {
+          sum = sum + gold.points
+        }
+      })
+      event.silver.map(silver => {
+        if (silver.id === country._id) {
+          sum = sum + silver.points
+        }
+      })
+      event.bronze.map(bronze => {
+        if (bronze.id === country._id) {
+          sum = sum + bronze.points
+        }
+      })
+    })
+    country.points = sum
+    return country
+  }
+
+  sortByPoints(countries) {
+    if (countries.length) {
+      return countries.sort(function(a, b) {
+        if(a.points < b.points) return 1
+        if(a.points > b.points) return -1
+        return 0
+      })
+    } else {
+      return null
+    }
+  }
+
+  render() {
+    const { userId } = this.props
+    let renderCountries = []
+
+    const userCountries = this.findUserCountries(userId)
+    if (userCountries.length > 0) {
+      const summedCountries = userCountries.map((country, index) => {
+        return this.sumCountry(country)
+      })
+      const sortedCountries = this.sortByPoints(summedCountries)
+      renderCountries = sortedCountries.map((country, index) => {
+        return (
+          <li key={index}>
+            <div className="user-country-content">
+              <div className="name-rank">
+                <span className="rank">{index + 1}</span>
+                <span className="name"><Flag country={country}/>{country.name}</span>
+              </div>
+              <span className="points">{country.points}</span>
+            </div>
+          </li>
+        )
+      })
+      return (
+        <ul>
+          {renderCountries}
+        </ul>
+      )
+    } else {
+      renderCountries.push(
+        <li key={0}>
+          <div className="user-country-content">
+            <span className="no-drafts">No Countries Drafted</span>
+          </div>
+        </li>
+      )
+      return (
+        <ul>
+          {renderCountries}
+        </ul>
+      )
+    }
+  }
+}
