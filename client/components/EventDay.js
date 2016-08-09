3f25232c0a22e3da3703e9b1cbaf5eb2bcc4fcef
John Heher <jheher@gmail.com>
2016-08-09 00:15:41 -0500

    Adds player of the day

--- client/components/EventDay.js
+++ client/components/EventDay.js
@@ -51,6 +51,64 @@ export default class EventDay extends React.Component {
     return moment(datetime, "YYYY-MM-DD").format("ddd, M/D")
   }
 
+  findUserCountries(userId) {
+    return this.props.countries.filter(country => {
+      return country.userId === userId
+    })
+  }
+
+  sumCountry(country, events) {
+    let sum = 0
+    events.map(event => {
+      event.gold.map(gold => {
+        if (gold.id === country._id) {
+          sum = sum + gold.points
+        }
+      })
+      event.silver.map(silver => {
+        if (silver.id === country._id) {
+          sum = sum + silver.points
+        }
+      })
+      event.bronze.map(bronze => {
+        if (bronze.id === country._id) {
+          sum = sum + bronze.points
+        }
+      })
+    })
+    country.points = sum
+    return country
+  }
+
+  renderPlayerOfDay(events) {
+    let topPlayer = {
+      name: "",
+      points: 0
+    }
+    this.props.paidUsers.map((user, index) => {
+      const userCountries = this.findUserCountries(user._id)
+      let userCountrySum = 0
+      userCountries.map(country => {
+        const summedCountry = this.sumCountry(country, events)
+        userCountrySum = userCountrySum + country.points
+      })
+      if (userCountrySum > topPlayer.points) {
+        topPlayer.name = user.name
+        topPlayer.points = userCountrySum
+      }
+    })
+    if (topPlayer.points > 0) {
+      return (
+        <div className="player-of-day">
+          <span className="title">Player of the Day</span>
+          <p className="player">{topPlayer.name} <span>{topPlayer.points}</span></p>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
   render() {
     const sortedEvents = this.sortEvents(this.props.eventGroup)
     const events = sortedEvents.map((event, index) => {
@@ -59,9 +117,12 @@ export default class EventDay extends React.Component {
 
     return (
       <div className={`event-day ${this.state.showEvents ? "show" : "hide"}`}>
-        <div className="title">
-          <h2 onClick={this.toggleEvents.bind(this)}>{this.convertDate(this.props.title)}</h2>
-          {this.showToggle()}
+        <div className="day-title">
+          <div className="header">
+            <h2 onClick={this.toggleEvents.bind(this)}>{this.convertDate(this.props.title)}</h2>
+            {this.showToggle()}
+          </div>
+          {this.renderPlayerOfDay(this.props.eventGroup)}
         </div>
         {events}
       </div>
